6550bcd5b69bca9f45c12aad56336a98c45e5c10
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-02-01 12:26:01 +0100

    fix: rename seed folder to seeds to be dettected by flask seeder

---
+++ seeds/role_seeds.py
@@ -0,0 +1,9 @@
+from flask_seeder import Seeder
+from peskos.models.roles import Role
+
+class RoleSeeder(Seeder):
+    def run(self):
+        roles = [Role(role="superadmin"), Role(role="admin")]
+        for role in roles:
+            print(f"Added {role}")
+            self.db.session.add(role)
